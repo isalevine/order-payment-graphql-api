799e295be4878da9c87b4d6823dc391cbae5180d
isalevine <isa.loves.coding@gmail.com>
2019-09-14 23:11:31 -0700

    Fixed typo in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ order2 = Order.create(description: "Mega Man 3 OST", total: 29.99, reference_key
 order3 = Order.create(description: "Punch Out!! NES", total: 0.75, reference_key: SecureRandom.uuid)
 
 payment1 = Payment.create(amount: 10.00, note: "First payment", idempotency_key: SecureRandom.uuid)
-payment1 = Payment.create(amount: 10.00, note: "", idempotency_key: SecureRandom.uuid)
-payment1 = Payment.create(amount: 10.00, idempotency_key: SecureRandom.uuid)
+payment2 = Payment.create(amount: 10.00, note: "", idempotency_key: SecureRandom.uuid)
+payment3 = Payment.create(amount: 10.00, idempotency_key: SecureRandom.uuid)
 
 pending1 = PendingOrderPayment.create(order_id: order1.id, payment_id: payment1.id, idempotency_key: payment1.idempotency_key, status: "Successful")
 pending2 = PendingOrderPayment.create(order_id: order2.id, payment_id: payment2.id, idempotency_key: payment2.idempotency_key, status: "Pending")
