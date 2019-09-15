755d07e34869361a7a56b05f94df9f39d4ea1816
isalevine <isa.loves.coding@gmail.com>
2019-09-15 12:44:52 -0700

    Refactored CreatePayment mutation to modularize steps in resolve() and make code DRYer

--- app/graphql/mutations/create_payment.rb
+++ app/graphql/mutations/create_payment.rb
@@ -7,6 +7,7 @@ class Mutations::CreatePayment < Mutations::BaseMutation
     field :errors, [String], null: false
 
 
+
     def resolve(reference_key:, amount:, note: nil)
         # Order (for successful payments):
         #   1. Check for existing idempotency_key
@@ -18,11 +19,23 @@ class Mutations::CreatePayment < Mutations::BaseMutation
         #   7. Return Order, with new Payment listed inside successful_payments field
 
 
+        # Is there a better way to organize + pass around Mutation arguments?
+        paymentDataHash = {
+            reference_key: reference_key,
+            amount: amount,
+            note: note
+        }
+        return lookupPaymentIdempotencyKey(paymentDataHash)
+    end
+
+
+
+    # Helper functions for resolve()
+
+    def lookupPaymentIdempotencyKey(paymentDataHash)
         # New instances of the createPayment mutation will generate unique idempotency_key (UUID)
         idempotency_key = SecureRandom.uuid     # change this to a non-random String to test if idempotency_key match-found error is thrown (currently: yes!)
         
-
-    
         # Check if idempotency_key already exists -- if so, transaction is a duplicate!
         pendingOrderPayment = PendingOrderPayment.find_by(idempotency_key: idempotency_key)
         if pendingOrderPayment
@@ -33,56 +46,63 @@ class Mutations::CreatePayment < Mutations::BaseMutation
                     order: nil,
                     errors: ["PendingOrderPayment with status '#{pendingOrderPayment.status}' and matching idempotency_key detected -- payment declined!"]
                 }
-            
             # Payment not applied -- retry updating Order's balanceDue
             elsif pendingOrderPayment.status == "Pending" || pendingOrderPayment.status == "Failed"
-                # Attempt to apply amount to Order's balanceDue again => MODULARIZE!
+                return lookupOrderReferenceKey(paymentDataHash, idempotency_key: idempotency_key, pendingOrderPayment: pendingOrderPayment)
             end
-
 
         # No idempotency_key match found -- look up Order by reference_key to apply Payment
         else
-            order = Order.find_by(reference_key: reference_key)
-
-            # Order successfully found -- create new Payment and PendingOrderPayment,
-            # and test if Payment will change Order's balanceDue by expected amount
-            if order
-                payment = Payment.create(amount: amount, note: note, idempotency_key: idempotency_key)
-                pendingOrderPayment = PendingOrderPayment.create(order_id: order.id, payment_id: payment.id, idempotency_key: idempotency_key, status: "Pending")
-                
-                starting_balance = order.balance_due
-                expected_balance = starting_balance - payment.amount
-
-                pendingOrderPayment.status = "Successful"
-                pendingOrderPayment.save
-                
-                # If balanceDue is expected value, return Order -- successfulPayments will include new Payment
-                if order.balance_due == expected_balance
-                    return {
-                        order: order,
-                        errors: []
-                    }
-
-                # balanceDue is not expected value -- change status to "Failed" and decline Payment
-                else
-                    pendingOrderPayment.status = "Failed"
-                    pendingOrderPayment.save
-                    return {
-                        order: nil,
-                        errors: ["Unexpected value for Order's balanceDue -- PendingOrderPayment status is now '#{pendingOrderPayment.status}' -- payment declined!"]
-                    }
-                end
+            return lookupOrderReferenceKey(paymentDataHash, idempotency_key)
+        end
+    end
 
 
-            # No Order found
-            else
-                return {
-                    order: nil,
-                    errors: ["Order with reference_key #{reference_key} not found -- payment declined!"]
-                }
-            end
+    def lookupOrderReferenceKey(paymentDataHash, idempotency_key, pendingOrderPayment: nil)
+        order = Order.find_by(reference_key: paymentDataHash[:reference_key])
 
+        # Order successfully found -- create new Payment and PendingOrderPayment,
+        # and test if Payment will change Order's balanceDue by expected amount
+        if order
+            return applyPaymentToOrder(paymentDataHash, order, idempotency_key, pendingOrderPayment: pendingOrderPayment)
+
+        # No Order found -- return errors
+        else
+            return {
+                order: nil,
+                errors: ["Order with reference_key #{reference_key} not found -- payment declined!"]
+            }
+        end
+    end
+
+
+    def applyPaymentToOrder(paymentDataHash, order, idempotency_key, pendingOrderPayment: nil)
+        payment = Payment.create(amount: paymentDataHash[:amount], note: paymentDataHash[:note], idempotency_key: idempotency_key)
+        if !pendingOrderPayment
+            pendingOrderPayment = PendingOrderPayment.create(order_id: order.id, payment_id: payment.id, idempotency_key: idempotency_key, status: "Pending")
         end
 
+        starting_balance = order.balance_due
+        expected_balance = starting_balance - payment.amount
+
+        pendingOrderPayment.status = "Successful"
+        pendingOrderPayment.save
+        
+        # If balanceDue is expected value, return Order -- successfulPayments will include new Payment
+        if order.balance_due == expected_balance
+            return {
+                order: order,
+                errors: []
+            }
+
+        # balanceDue is not expected value -- change status to "Failed" and decline Payment
+        else
+            pendingOrderPayment.status = "Failed"
+            pendingOrderPayment.save
+            return {
+                order: nil,
+                errors: ["Unexpected value for Order's balanceDue -- PendingOrderPayment status is now '#{pendingOrderPayment.status}' -- payment declined!"]
+            }
+        end
     end
 end
